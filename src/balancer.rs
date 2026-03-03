75fcb8de08ca69615df7347d3eebf54e82dd6764
Biruk-gebru <birukjember2004@gmail.com>
2026-03-03 20:02:24 +0300

    doc: added comment for algorithm used

--- src/balancer.rs
+++ src/balancer.rs
@@ -12,6 +12,8 @@ pub struct GateWayState {
 
 impl GateWayState {
     pub fn next_backend(&self) -> &str {
+        //round robin
+        //chore: will make this better with better alorthims 
         let index = self.counter.fetch_add(1, Ordering::Relaxed) % self.backends.len();//atomic adding and return the index
         &self.backends[index]
     }
