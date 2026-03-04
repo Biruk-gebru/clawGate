ea79b76a076a281b6cc5d91ed913d93eabdc0eb9
Biruk-gebru <birukjember2004@gmail.com>
2026-03-04 11:39:11 +0300

    feat: added an auth module

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,42 @@
+// mod declarations
 mod proxy;
 mod balancer;
+mod middleware;
+
+// crate imports
 use crate::balancer::{SharedState, GateWayState};
 use crate::proxy::proxy_request;
+use crate::middleware::auth::require_auth;
+
+// dependency imports
 use axum::{
     Router,
 };
 use reqwest::Client;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
+use tower_http::trace::TraceLayer;
+
 
 #[tokio::main]
 async fn main() {
     //creating shared state
     let state = Arc::new(GateWayState {
-    backends: vec![
-        "http://127.0.0.1:4000".to_string(),
-        "http://127.0.0.1:4001".to_string(),
-    ],
-    counter: AtomicUsize::new(0),
-    client: Client::new(),
+        backends: vec![
+            "http://127.0.0.1:4000".to_string(),
+            "http://127.0.0.1:4001".to_string(),
+        ],
+        counter: AtomicUsize::new(0),
+        client: Client::new(),
     });
-
+    
+    tracing_subscriber::fmt().init();
     //route create 
     let app = Router::new()
-    .fallback(proxy_request).with_state(state);
+        .fallback(proxy_request)
+        .with_state(state)
+        .layer(middleware::from_fn(require_auth))
+        .layer(TraceLayer::new_for_http());
 
     let listner = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listner, app).await.unwrap();
---
+++ src/middleware/auth.rs
@@ -0,0 +1,18 @@
+use axum::extract::Request;
+use axum::http::StatusCode;
+use axum::middleware::Next;
+use axum::response::IntoResponse;
+
+pub async fn require_auth(request: Request, next: Next) -> impl IntoResponse {
+    let auth_header = request.headers().get("Authorization");
+
+    match auth_header {
+        Some(header) => {
+            header.next.run(request).await
+        }
+        None => {
+            StatusCode::UNAUTHORIZED.into_response()
+        }
+    }
+}
+
