eefd5e271fab70152516e317c748b2c10d936904
Biruk-gebru <birukjember2004.com>
2026-03-04 18:46:25 +0300

    fix: added a buffer so that axum get an infalluble type

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,19 @@ use crate::middleware::auth::require_auth;
 use axum::{
     Router,
 };
+use axum::middleware::from_fn;
+use axum::error_handling::HandleErrorLayer;
 use reqwest::Client;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 use tower_http::trace::TraceLayer;
 use std::time::Duration;
 use tower::limit::RateLimitLayer;
+use tower::ServiceBuilder;
+use tower::buffer::BufferLayer;
+use tower::BoxError;
+use reqwest::StatusCode;
+
 
 
 #[tokio::main]
@@ -37,8 +44,11 @@ async fn main() {
     let app = Router::new()
         .fallback(proxy_request)
         .with_state(state)
-        .layer(middleware::from_fn(require_auth))
-        .layer(RateLimitLayer::new(100, Duration::from_secs(1)))//chore:: make this global rate limiting specific to an ip
+        .layer(from_fn(require_auth))
+        .layer(ServiceBuilder::new()  
+            .layer(HandleErrorLayer::new(|_: BoxError| async {StatusCode::SERVICE_UNAVAILABLE}))
+            .layer(BufferLayer::new(1024))//add a buffer so axum gets a Clone type
+            .layer(RateLimitLayer::new(1, Duration::from_secs(1))))//chore:: make this global rate limiting specific to an ip)
         .layer(TraceLayer::new_for_http());
 
     let listner = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
